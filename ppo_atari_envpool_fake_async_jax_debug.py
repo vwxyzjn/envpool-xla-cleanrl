8f391d8c871d9259306e27dbaba7727e745c61fc
Costa Huang <costa.huang@outlook.com>
2022-07-26 22:55:48 -0400

    push

--- ppo_atari_envpool_fake_async_jax_debug.py
+++ ppo_atari_envpool_fake_async_jax_debug.py
@@ -365,6 +365,9 @@ if __name__ == "__main__":
         return logprob, entropy, value
 
     take_idx_fn = jax.vmap(lambda x, idxs: x[idxs]) # TODO: check if this is correct
+
+    revert_idx_fn = jax.vmap(lambda src, target, idxs: target.at[idxs].set(src))
+    
     @jax.jit
     def compute_gae(
         rewards: np.ndarray,
@@ -373,9 +376,16 @@ if __name__ == "__main__":
         env_ids: np.ndarray,
     ):
         env_ids = jnp.asarray(env_ids).reshape(args.num_steps + 1, -1)
-        rewards = take_idx_fn(jnp.asarray(rewards).reshape(args.num_steps + 1, -1), env_ids)[1:]  # NOTE: handle rewards off by 1 w/ [1:]
-        values = take_idx_fn(jnp.asarray(values).reshape(args.num_steps + 1, -1), env_ids)
-        dones = take_idx_fn(jnp.asarray(dones).reshape(args.num_steps + 1, -1), env_ids)
+        rewards = jnp.asarray(rewards).reshape(args.num_steps + 1, -1)
+        values = jnp.asarray(values).reshape(args.num_steps + 1, -1)
+        dones = jnp.asarray(dones).reshape(args.num_steps + 1, -1)
+
+        rewards = revert_idx_fn(rewards, jnp.zeros_like(rewards), env_ids)
+        values = revert_idx_fn(values, jnp.zeros_like(values), env_ids)
+        dones = revert_idx_fn(dones, jnp.zeros_like(dones), env_ids)
+        # rewards = take_idx_fn(jnp.asarray(rewards).reshape(args.num_steps + 1, -1), env_ids)[1:]  # NOTE: handle rewards off by 1 w/ [1:]
+        # values = take_idx_fn(jnp.asarray(values).reshape(args.num_steps + 1, -1), env_ids)
+        # dones = take_idx_fn(jnp.asarray(dones).reshape(args.num_steps + 1, -1), env_ids)
 
         next_value = values[-1]
         next_done = dones[-1]
@@ -407,9 +417,12 @@ if __name__ == "__main__":
         key: jax.random.PRNGKey,
     ):
         env_ids = jnp.asarray(env_ids).reshape(args.num_steps + 1, -1)
-        b_obs = take_idx_fn(jnp.asarray(obs).reshape((args.num_steps + 1, -1,)+ envs.single_observation_space.shape), env_ids)[:-1].reshape((-1,) + envs.single_observation_space.shape)
-        b_logprobs = take_idx_fn(jnp.asarray(logprobs).reshape(args.num_steps + 1, -1), env_ids)[:-1].reshape(-1)
-        b_actions = take_idx_fn(jnp.asarray(actions).reshape(args.num_steps + 1, -1), env_ids)[:-1].reshape((-1,) + envs.single_action_space.shape)
+        obs = jnp.asarray(obs).reshape((args.num_steps + 1, -1,)+ envs.single_observation_space.shape)
+        logprobs =jnp.asarray(logprobs).reshape(args.num_steps + 1, -1)
+        actions = jnp.asarray(actions).reshape(args.num_steps + 1, -1)
+        b_obs = revert_idx_fn(obs, jnp.zeros_like(obs), env_ids)[:-1].reshape((-1,) + envs.single_observation_space.shape)
+        b_logprobs = revert_idx_fn(logprobs, jnp.zeros_like(logprobs), env_ids)[:-1].reshape(-1)
+        b_actions = revert_idx_fn(actions, jnp.zeros_like(actions), env_ids)[:-1].reshape((-1,) + envs.single_action_space.shape)
         b_advantages = advantages.reshape(-1)
         b_returns = returns.reshape(-1)
 
@@ -492,7 +505,7 @@ if __name__ == "__main__":
         logprobs.append(logprob)
         env_ids.append(env_id)
         rewards.append(next_reward)
-    # raise
+
     for update in range(1, args.num_updates + 2):
         # roll over data from last rollout phase
         obs = obs[-async_update:]
@@ -530,7 +543,7 @@ if __name__ == "__main__":
         avg_episodic_return = np.mean(returned_episode_returns)
         print(f"global_step={global_step}, avg_episodic_return={avg_episodic_return}")
         writer.add_scalar("charts/avg_episodic_return", avg_episodic_return, global_step)
-        # NOTE!!!!!!!!!!!!! what is env_ids for the rewards?
+
         advantages, returns = compute_gae(rewards, values, dones, env_ids)
         print(key)
         print("actions.sum()", np.array(actions).sum())
@@ -538,7 +551,7 @@ if __name__ == "__main__":
         print("returns.sum()", np.array(returns).sum())
         print("dones.sum()", np.array(dones).sum())
         print("envs.envs[0].last_actions", envs.envs[0].last_actions)
-        raise
+
         agent_state, loss, pg_loss, v_loss, entropy_loss, approx_kl, _ = update_ppo(  # TODO: fix key
             agent_state,
             obs,
@@ -551,14 +564,16 @@ if __name__ == "__main__":
             env_ids,
             key,
         )
-
-        print("v_loss", v_loss)
-        if update == 1:
-            raise
-        # raise
-        # print("network_params", agent_state.params.network_params["params"]["Dense_0"]["kernel"].sum())
-        # print("actor_params", agent_state.params.actor_params["params"]["Dense_0"]["kernel"].sum())
-        # print("critic_params", agent_state.params.critic_params["params"]["Dense_0"]["kernel"].sum())
+        print("network_params", agent_state.params.network_params["params"]["Dense_0"]["kernel"].sum())
+        print("actor_params", agent_state.params.actor_params["params"]["Dense_0"]["kernel"].sum())
+        print("critic_params", agent_state.params.critic_params["params"]["Dense_0"]["kernel"].sum())
+        
+        print('loss', loss)
+        print('pg_loss', pg_loss)
+        print('v_loss', v_loss)
+        print('entropy_loss', entropy_loss)
+        print('approx_kl', approx_kl)
+        raise
         writer.add_scalar("stats/advantages", advantages.mean().item(), global_step)
         writer.add_scalar("stats/returns", returns.mean().item(), global_step)
 
